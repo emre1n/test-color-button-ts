605b27317f3c321cea98804421b7c02898a8897e
emre1n <emre1n@protonmail.com>
2023-04-15 16:51:37 +0300

    add test and code of checkbox functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,28 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   const colorChangeHandler = () => setButtonColor(newButtonColor);
+  const checkboxChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setDisabled(e.target.checked);
 
   return (
     <>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={colorChangeHandler}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={checkboxChangeHandler}
+      />
     </>
   );
 }
